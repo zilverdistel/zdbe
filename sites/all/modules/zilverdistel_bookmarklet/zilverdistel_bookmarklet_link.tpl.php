56a91b8aceb5c3c3048ff84cfa4dcf8b72c1df7f
zilverdistel <zilverdistel@zilverdistel.be>
2013-02-18 23:50:04 +0100

    Provided myself with a bookmarklet to post content.

---
+++ sites/all/modules/zilverdistel_bookmarklet/zilverdistel_bookmarklet_link.tpl.php
@@ -0,0 +1,27 @@
+Link: <a href='javascript:
+var nodeaddpath="http://phoenix.local/dev/zilverdistel.be/?q=node/add/feed-item",
+  d=document,
+  w=window,
+  e=w.getSelection,
+  k=d.getSelection,
+  x=d.selection,
+  s=e?e():k?k():x?x.createRange().text:0,
+  l=d.location,
+  e=encodeURIComponent,
+  title=""+d.title+"",
+  body=""+s+"",
+  url=nodeaddpath+"&edit[title]="+e(title)+"&edit[body][und][0][value]="+e(body)+"&edit[field_original_link][und][0][url]="+l
+;
+console.log(e);
+nourl=function() {
+	w.location=nodeaddpath
+}
+;
+a=function() {
+	w.open(url,"quickpost","toolbar=0,resizable=1,scrollbars=1,status=1,width=1024,height=570")||(l.href=url)
+}
+;
+/Firefox/.test(navigator.userAgent)?"about:blank"==l?setTimeout(nourl,0):setTimeout(a,0):"about:blank"==l?nourl():a();
+;
+void(0);
+'>Post this!</a>
